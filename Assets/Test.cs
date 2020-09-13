482a5a59d4578abbe561d1e26bb792b14ccbf78a
sotetsu02 <70087180+sotetsu02@users.noreply.github.com>
2020-09-13 14:28:03 +0900

    Initial commit

---
+++ Assets/Test.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int[] array = new int[5];
+        array[0] = 10;
+        array[1] = 20;
+        array[2] = 30;
+        array[3] = 50;
+        array[4] = 40;
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            UnityEngine.Debug.Log(array[i]);
+        }
+        for (int i = 4; i >= 0; i--)
+        {
+            UnityEngine.Debug.Log(array[i]);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Test2.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss
+{
+    private int mp = 53;
+
+    //魔法攻撃関数
+    public void Magic(int cost)
+    {
+        if (this.mp >= 5)
+        {
+            cost = this.mp - 5;
+            UnityEngine.Debug.Log("魔法攻撃をした。残りＭＰは" + cost + "。");
+        }
+        else
+        {
+            UnityEngine.Debug.Log("ＭＰが足りないため魔法が使えない。");
+        }
+    }
+}
+
+public class Test2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Boss lastboss = new Boss();
+
+        lastboss.Magic(0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
